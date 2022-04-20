441ebd0914297cb7594af0ad965f6e11a0cd00c9
doggai10 <doggai77@gmail.com>
2022-04-21 02:14:05 +0900

    5장 spring security & oAuth2.0

--- src/test/java/com/test/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/test/book/springboot/web/HelloControllerTest.java
@@ -1,9 +1,13 @@
 package com.test.book.springboot.web;
 
+import com.test.book.springboot.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -12,12 +16,19 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest(controllers = HelloController.class,
+    excludeFilters = {
+        @ComponentScan.Filter(type= FilterType.ASSIGNABLE_TYPE,
+            classes = SecurityConfig.class
+        )
+    }
+)
 public class HelloControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
+    @WithMockUser(roles="USER")
     @Test
     public void helloWillReturn() throws Exception {
         String hello = "hello";
@@ -27,6 +38,7 @@ public class HelloControllerTest {
                 andExpect(content().string(hello));
     }
 
+    @WithMockUser(roles="USER")
     @Test
     public void helloDtoWillReturn() throws Exception {
         String name = "hello";
@@ -37,9 +49,5 @@ public class HelloControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.name", is(name)))
                 .andExpect(jsonPath("$.amount", is(amount)));
-
-
-
-
     }
 }
