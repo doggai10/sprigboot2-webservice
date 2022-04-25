1c146b6acb1b41f1b96ca2d99df5f31380b56682
doggai10 <doggai77@gmail.com>
2022-04-26 02:31:34 +0900

    update 10장 start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -11,8 +11,8 @@ echo "> Build 파일 복사"
 echo "cp $REPOSITORY/$PROJECT_NAME/build/libs/*.jar $REPOSITORY/"
 #echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY/"
 
-#cp $REPOSITORY/zip/*.jar $REPOSITORY/
 cp $REPOSITORY/$PROJECT_NAME/build/libs/*.jar $REPOSITORY/
+#cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 새 어플리케이션 배포"
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
