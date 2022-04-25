23e2594fdb1ebff5599ac7f26d7200007b17414f
doggai10 <doggai77@gmail.com>
2022-04-26 02:40:08 +0900

    final commit v1

--- scripts/start.sh
+++ scripts/start.sh
@@ -8,9 +8,11 @@ REPOSITORY=/home/ec2-user/app/step3
 PROJECT_NAME=springboot2-webservice
 
 echo "> Build 파일 복사"
-echo "cp $REPOSITORY/$PROJECT_NAME/build/libs/*.jar $REPOSITORY/"
+#echo "cp $REPOSITORY/$PROJECT_NAME/build/libs/*.jar $REPOSITORY/"
+echo "cp $REPOSITORY/$PROJECT_NAME/*.jar $REPOSITORY/"
 
-cp $REPOSITORY/$PROJECT_NAME/build/libs/*.jar $REPOSITORY/
+#cp $REPOSITORY/$PROJECT_NAME/build/libs/*.jar $REPOSITORY/
+cp $REPOSITORY/$PROJECT_NAME/build/*.jar $REPOSITORY/
 
 echo "> 새 어플리케이션 배포"
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
